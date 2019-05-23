cb8eedf6ce4b2d1c9266d1dbaaa2790f9923d039
Melissa <nolascm1@gmail.com>
2019-05-23 18:27:52 +0000

    Heroku App

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
 
+  config.assets.initialize_on_precompile = false
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
