6e928cfe34b3d0e33d9c3d4abe7321f4044e6ec7
Melissa <nolascm1@gmail.com>
2019-05-23 18:33:15 +0000

    heroku 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'nomnoms-melissa-nolasco.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
-end
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -93,3 +93,6 @@ end
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+
+#config.assets.initialize_on_precompile = false
