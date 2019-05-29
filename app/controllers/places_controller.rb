8d36cf93bb940df73ed013d3c7b7cfa0d7b4c11c
Melissa <nolascm1@gmail.com>
2019-05-29 22:50:35 +0000

    Lesson 27

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
     def index
       @places = Place.all.paginate(page: params[:page], per_page: 1)
@@ -20,16 +20,29 @@ class PlacesController < ApplicationController
 
     def edit
         @place = Place.find(params[:id])
+
+        if @place.user != current_user
+            return render plain: 'NomNomers Only', status: :forbidden
+        end
     end
 
     def update
         @place = Place.find(params[:id])
+        if @place.user != current_user
+            return render plain: 'NomNomers Only', status: :forbidden
+        end
+
+
         @place.update_attributes(place_params)
         redirect_to root_path
     end
 
     def destroy
         @place = Place.find(params[:id])
+        if @place.user != current_user
+            return render plain: 'Not Allowed', status: :forbidden
+        end
+
         @place.destroy
         redirect_to root_path
     end
