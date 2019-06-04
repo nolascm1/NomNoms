2565058ac097ff2bc6c42a8df0a9788f8caa3724
Melissa <nolascm1@gmail.com>
2019-06-04 19:23:14 +0000

    geocoding address

--- Gemfile
+++ Gemfile
@@ -69,12 +69,16 @@ gem 'simple_form'
 gem "font-awesome-rails"
 gem 'devise'
 gem 'will_paginate', '~> 3.1', '>= 3.1.7'
+gem "geocoder"
+gem "figaro"
 
 
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
+
+
 
 
 
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,16 @@
 class Place < ApplicationRecord
     belongs_to :user
-    validates :name, presence: true
+    geocoded_by :address
+    after_validation :geocode
+
+    validates :name, :address, :description, presence: true
+    validates :name, length: { minimum: 3 }
 end
+
+
+# class Person < ApplicationRecord
+#   validates :name, length: { minimum: 2 }
+#   validates :bio, length: { maximum: 500 }
+#   validates :password, length: { in: 6..20 }
+#   validates :registration_number, length: { is: 6 }
+# end
