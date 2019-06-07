234a3769cc48a8d614ab1bc8bd876a8438ee2aa0
Melissa <nolascm1@gmail.com>
2019-06-07 20:18:10 +0000

    tryingstill

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ApplicationRecord
     belongs_to :user
     has_many :comments
-    has_many :photos
+    belongs_to :photos
     
     geocoded_by :address
     after_validation :geocode
