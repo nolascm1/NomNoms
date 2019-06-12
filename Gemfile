1f8cff4d3ec3f273bd82778cf6279e5957482f36
Melissa <nolascm1@gmail.com>
2019-06-12 21:31:13 +0000

    Lesson 45

--- Gemfile
+++ Gemfile
@@ -74,7 +74,6 @@ gem "figaro"
 gem 'carrierwave', '~> 1.0'
 
 
-
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -10,10 +10,3 @@ class Place < ApplicationRecord
     validates :name, length: { minimum: 3 }
 end
 
-
-# class Person < ApplicationRecord
-#   validates :name, length: { minimum: 2 }
-#   validates :bio, length: { maximum: 500 }
-#   validates :password, length: { in: 6..20 }
-#   validates :registration_number, length: { is: 6 }
-# end
